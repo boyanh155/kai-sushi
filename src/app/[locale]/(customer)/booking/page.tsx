0e04c7ef566e20cfe871b70c784ad0751715a862
locle <locle2001@gmail.com>
2024-03-27 23:33:15 +0700

    Finish input float

--- src/app/[locale]/(customer)/booking/page.tsx
+++ src/app/[locale]/(customer)/booking/page.tsx
@@ -28,7 +28,7 @@ const currencies = [
 const BookingPage = (props: Props) => {
   const t = useTranslations("Booking");
   return (
-    <div className="flex flex-col content-container">
+    <div className="flex flex-col content-container items-center px-7">
       {/* IMAGE */}
       <div className="w-[100px] h-[100px]">
         <Image src={logoSquare} className="w-full h-auto" alt="Kai logo" />
@@ -37,22 +37,49 @@ const BookingPage = (props: Props) => {
         {t("booking")}
       </h2>
       {/* Button group */}
-      <div className="flex flex-col mt-16">
+      <div className="flex flex-col mt-16 w-full ">
         {/* Adult */}
-        <TextField
-          id="outlined-select-currency"
-          select
-          label="Select"
-          defaultValue="EUR"
-          className="border-[0.4px] border-[#959595]"
-          helperText="Please select your currency"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+        {/* Name */}
+        <div className="input-float">
+          <input
+            id="name"
+            name="name"
+            type="text"
+            className=""
+            placeholder="Enter your name"
+          />{" "}
+          <label htmlFor="name" id="labelName">
+            Name
+            <p>*</p>
+          </label>
+        </div>
+        {/* Phone */}
+        <div className="input-float mt-11">
+          <input
+            id="phone"
+            name="phone"
+            type="text"
+            className=""
+            placeholder="+84"
+          />{" "}
+          <label htmlFor="name" id="labelName">
+            Phone
+            <p>*</p>
+          </label>
+        </div>
+        {/* Is notify */}
+        <div className="mt-3 flex items-center">
+          <input
+            id="isNotify"
+            name="isNotify"
+            type="checkbox"
+            defaultChecked
+            className="checkbox w-3 h-3 border-white [--chkbg:theme(colors.indigo.600)]  border-[0.4px] rounded-sm"
+          />
+          <label htmlFor="isNotify" className=" ms-2  font-light text-sm">
+            Thông báo tôi qua tin nhắn?
+          </label>
+        </div>
       </div>
     </div>
   );
