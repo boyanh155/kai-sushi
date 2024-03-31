39ad32a97969ad0a1be98e185d2ef071988beb44
locle <locle2001@gmail.com>
2024-03-31 23:30:04 +0700

    add qr code booking

---
+++ src/libs/qrcode.ts
@@ -0,0 +1,33 @@
+import Jimp from "jimp";
+import path from "path";
+import QRCode from "qrcode";
+import fs from "fs";
+
+
+export async function generateQRCodeWithLogo(text: string, outputPath: string) {
+  // Generate QR code
+console.log(outputPath)
+  const qrCodeImage = await QRCode.toBuffer(text, { type: "png" });
+ 
+  // Read the QR code and the logo
+  const [qr, logo] = await Promise.all([
+    Jimp.read(qrCodeImage),
+    Jimp.read("./public/logo.png"),
+  ]);
+
+  // Resize the logo
+  logo.resize(qr.bitmap.width / 5, Jimp.AUTO);
+
+  // Calculate the position for the logo (center of the QR code)
+  const logoPos = {
+    x: (qr.bitmap.width - logo.bitmap.width) / 2,
+    y: (qr.bitmap.height - logo.bitmap.height) / 2,
+  };
+
+  // Composite the logo onto the QR code
+  qr.composite(logo, logoPos.x, logoPos.y);
+
+  // Save the final image
+  await qr.writeAsync(outputPath);
+}
+
