ee922abe892a3632a832ab9973ba8b5eb6d240cf
locle <locle2001@gmail.com>
2024-04-02 18:08:41 +0700

    fix loading menu

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import Loading from "@/components/shared/Loading";
+import React from "react";
+
+type Props = {};
+
+const LoadingPage = (props: Props) => {
+  return <Loading />;
+};
+
+export default LoadingPage;
