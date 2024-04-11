fc09d4d5332f0e23cf80f3620f4b2a18c838e92d
loclh <loclh@wikiland.vn>
2024-04-11 14:39:26 +0700

    fix date

--- src/app/[locale]/(customer)/booking/order-info/page.tsx
+++ src/app/[locale]/(customer)/booking/order-info/page.tsx
@@ -83,13 +83,54 @@ const OrderInfoPage = () => {
     );
     const _minTime = moment(today + " " + minTime, "YYYY-MM-DD HH:mm");
     const _maxTime = moment(today + " " + maxTime, "YYYY-MM-DD HH:mm");
-    console.log(today + " " + minTime);
-    console.log(_minTime.toDate());
-    console.log(today + " " + maxTime);
 
-    console.log(_maxTime.toDate());
+    // if (
+    //   !(
+    //     moment(selectedDate, "YYYY-MM-DD").isSame(moment(), "day") &&
+    //     _selectedTime.isBetween(_minTime, _maxTime, undefined, "[]")
+    //   )
+    // ) {
+    //   console.log("aa");
+    //   setIsInvalid(true);
+    //   return;
+    // } else if (
+    //   !(
+    //     _selectedTime.isBetween(
+    //       moment("16:00", "HH:mm"),
+    //       _maxTime,
+    //       undefined,
+    //       "[]"
+    //     ) &&
+    //     moment(selectedDate, "YYYY-MM-DD").isBetween(
+    //       moment(minDate, "YYYY-MM-DD"),
+    //       moment(maxDate, "YYYY-MM-DD"),
+    //       undefined,
+    //       "(]"
+    //     )
+    //   )
+    // ) {
+    //   console.log(
+    //     _selectedTime.isBetween(
+    //       moment("16:00", "HH:mm"),
+    //       _maxTime,
+    //       undefined,
+    //       "[]"
+    //     )
+    //   );
+    //   console.log(
+    //     moment(selectedDate, "YYYY-MM-DD").isBetween(
+    //       moment(minDate, "YYYY-MM-DD"),
+    //       moment(maxDate, "YYYY-MM-DD"),
+    //       undefined,
+    //       "[]"
+    //     )
+    //   );
+    //   setIsInvalid(true);
+    //   return;
+    // }
+
+    // check date
     if (
-      !_selectedTime.isBetween(_minTime, _maxTime, undefined, "[]") ||
       !moment(selectedDate, "YYYY-MM-DD").isBetween(
         moment(minDate, "YYYY-MM-DD"),
         moment(maxDate, "YYYY-MM-DD"),
@@ -99,9 +140,27 @@ const OrderInfoPage = () => {
     ) {
       setIsInvalid(true);
       return;
-    } else {
-      setIsInvalid(false);
     }
+    if (moment(selectedDate, "YYYY-MM-DD").isSame(moment(), "day")) {
+      if (!_selectedTime.isBetween(_minTime, _maxTime, undefined, "[]")) {
+        setIsInvalid(true);
+        return;
+      }
+    } else {
+      if (
+        !_selectedTime.isBetween(
+          moment("16:00", "HH:mm"),
+          _maxTime,
+          undefined,
+          "[]"
+        )
+      ) {
+        setIsInvalid(true);
+        return;
+      }
+    }
+    setIsInvalid(false);
+
     _setBookingState({
       ...bookingState,
       bookDate: moment(
