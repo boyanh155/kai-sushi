42d6f0573540c930534c64433b3cad60d6f616a5
locle <locle2001@gmail.com>
2024-04-16 17:34:33 +0700

    fix cors

--- next.config.mjs
+++ next.config.mjs
@@ -2,26 +2,20 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://www.kaisushiandlounge.me/:path*",
-      },
-      {
-        source: "/api/:path*",
-        destination: "https://www.kaisushilounge.com/:path*",
-      },
-      {
-        source: "/api/:path*",
-        destination: "https://kai-sushi.vercel.app/:path*",
-      },
-      {
-        source: "/api/:path*",
-        destination: "https://kaisushilounge.com/:path*",
-      },
-    ];
-  },
+   async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
 };
 
 const withNextIntl = createNextIntlPlugin("./src/libs/i18n.ts");
