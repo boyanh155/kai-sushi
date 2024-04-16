0bb8a5de447f98de6d6c9fce0f6bf375448c1ddb
locle <locle2001@gmail.com>
2024-04-16 17:13:35 +0700

    aa

--- next.config.mjs
+++ next.config.mjs
@@ -2,24 +2,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [{
-            source: '/api/:path*',
-            destination: 'https://www.kaisushiandlounge.me/:path*',
-        }, ]
-    },
-    async rewrites() {
-        return [{
-            source: '/api/:path*',
-            destination: 'https://kai-sushi.vercel.app/:path*',
-        }, ]
-    },  
-     async rewrites() {
-        return [{
-            source: '/api/:path*',
-            destination: 'https://kaisushilounge.com/:path*',
-        }, ]
-    },
        async rewrites() {
         return [{
             source: '/api/:path*',
