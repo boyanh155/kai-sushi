6438d3e27438fb008f83bb93cc6f51d2e3c0864f
locle <locle2001@gmail.com>
2024-04-24 21:58:52 +0700

    A

--- src/app/[locale]/(customer)/lunch/page.tsx
+++ src/app/[locale]/(customer)/lunch/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { gideon } from "@/libs/GoogleFont";
 import React from "react";
 import { Link } from "@/navigation";
@@ -28,7 +28,6 @@ const data = [
 ];
 
 const LunchPage = () => {
-
   const t = useTranslations("Home");
   return (
     <div className="relative overflow-visible flex flex-col min-h-[calc(100vh-110px)] items-center content-container pb-24">
@@ -36,38 +35,36 @@ const LunchPage = () => {
       <div
         className={`w-screen uppercase text-4xl h-56  relative after:absolute after:w-full after:h-full after:bg-black after:inset-0 after:opacity-80 after:z-40 ${`bg-no-repeat bg-contain bg-center`} pb-28`}
       >
-        <div className="absolute z-50  flex justify-center items-center w-full h-full flex-col gap-10">
+        <div className="absolute  z-50  flex justify-center items-center w-full h-full flex-col gap-10">
           <Link
             href="/"
             className="bg-black px-2 py-2.5 outline outline-[1px] outline-[#878787a6] rounded-sm text-xl"
           >
             x
           </Link>
-          <h1 className={`uppercase ${gideon.className}`}>
-            {t("lunch_label")}
-          </h1>
+          <h1 className={`uppercase ${gideon.className}`}>{t("lunch")}</h1>
         </div>
       </div>
       {/* BODY */}
       <div className="flex flex-col text-white text-base gap-8 w-full justify-center items-center px-[30px]">
-        <div>{t("lunch_set")}</div>
+        <div className="mt-8">{t("lunch_set")}</div>
         {/* MENU */}
         <div className="flex flex-col gap-10 w-full">
           {!isEmpty(data) ? (
             data.map((v, id) => (
               <div className="w-full flex-col flex gap-3 " key={id}>
                 {/* CARD */}
-                <div className=" border-[0.4px] border-[#ffffffab] flex justify-between px-3 pt-4 pb-8 uppercase font-light">
+                <div className=" border-[0.4px] rounded-sm border-[#95959586] flex justify-between px-3 pt-4 pb-8 uppercase font-light">
                   <div>{v.title}</div>
                   <div>{v.price}</div>
                 </div>
-                <div className="w-full h-[120px] overflow-hidden rounded-sm">
+                <div className="w-full h-[120px] overflow-hidden rounded-sm relative">
                   <Image
-                  objectFit="cover"
-                  layout="fill"
+                    objectFit="cover"
+                    layout="fill"
                     src={v.image}
                     alt="lunch_img "
-                    className="w-full h-[140px] block object-cover"
+                    className="w-full h-[140px] block object-cover relative"
                   />
                 </div>
               </div>
