087d8e445bbf236dc80916234920ce58e5bb6332
boyanh155 <locle2001@gmail.com>
2024-04-25 12:40:06 +0700

    a

--- src/app/[locale]/(customer)/lunch/page.tsx
+++ src/app/[locale]/(customer)/lunch/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { gideon } from "@/libs/GoogleFont";
 import React from "react";
 import { Link } from "@/navigation";
@@ -14,21 +14,21 @@ const data = [
       "https://s3-alpha-sig.figma.com/img/45d0/65e3/a69543b49080f9277d598ae54676a0a5?Expires=1714953600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=S7VpTaxE06HWv7enw4sgr8-qdiQcZsIOiONL5MnIkx4nn3fDSxCLPLamKp7db3YGC8rmZw15O7W2z8Iqo5FROL6RyMkJAZbtcuFbD1TpWJ2yXb71-I3uCPQobPo~S0HGRB10dLD1CofTjjUsuycHlhaM2MjFxikObGARXauSIQf~jCFTm-0G7z6jA7EJGQiIMvxZ5nJT1FZwl~QpSU7QNcS78jjKqDaKVtvCvCeOgImdgucO0QS~8Ov1M8polavWEQgMBADcKbpfGRXKWFBqIykGuG8L9PW-37XeunNP5EutZXa6VZNYgEfqtBGy3NkKNfOWEDhZvHu~ocou79QMhA__",
   },
   {
-    title: "Cá hồi nướng sốt teriyaki",
-    price: "139",
+    title: "Heo chiên xù",
+    price: "129",
     image:
-      "https://s3-alpha-sig.figma.com/img/45d0/65e3/a69543b49080f9277d598ae54676a0a5?Expires=1714953600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=S7VpTaxE06HWv7enw4sgr8-qdiQcZsIOiONL5MnIkx4nn3fDSxCLPLamKp7db3YGC8rmZw15O7W2z8Iqo5FROL6RyMkJAZbtcuFbD1TpWJ2yXb71-I3uCPQobPo~S0HGRB10dLD1CofTjjUsuycHlhaM2MjFxikObGARXauSIQf~jCFTm-0G7z6jA7EJGQiIMvxZ5nJT1FZwl~QpSU7QNcS78jjKqDaKVtvCvCeOgImdgucO0QS~8Ov1M8polavWEQgMBADcKbpfGRXKWFBqIykGuG8L9PW-37XeunNP5EutZXa6VZNYgEfqtBGy3NkKNfOWEDhZvHu~ocou79QMhA__",
+      "https://s3-alpha-sig.figma.com/img/9b0f/209f/f05558b021e34f1abf56966c3b63a116?Expires=1714953600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dMCiYxxlpLF5btBpdnYjJKkYB5Zm0DiK-BCUZ8zLd1LrqxW72Dx05N7fy2v9NI6l9lx9~tH4WPE6S5bPvWt7hrhnzTHq7vxCcNM3PoUAzltqcuxqCUGtk5ufRlbSqiGA0hEp7SPIffzCL2ts~1-5LEK~Jcqhtu2Cs7zuUJCZqp59hjhj4WxJ1oB0HCf~QkHyc0NZfQd-xJDNbAAqHc9lEIGLfA4ZPmGtZKB1mO4StFukYoxRZAiahq4slgX7HgusPmjbVIyilpghVjuaot3yVYYpPdxKsi44nlG0Vq3GFE4hOc136cq6O8hYIx5viyC80IdVPVZYwcn~hmIPNfaCkQ__",
   },
   {
-    title: "Cá hồi nướng sốt teriyaki",
-    price: "139",
+    title: "Sushi mix",
+    price: "129",
     image:
-      "https://s3-alpha-sig.figma.com/img/45d0/65e3/a69543b49080f9277d598ae54676a0a5?Expires=1714953600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=S7VpTaxE06HWv7enw4sgr8-qdiQcZsIOiONL5MnIkx4nn3fDSxCLPLamKp7db3YGC8rmZw15O7W2z8Iqo5FROL6RyMkJAZbtcuFbD1TpWJ2yXb71-I3uCPQobPo~S0HGRB10dLD1CofTjjUsuycHlhaM2MjFxikObGARXauSIQf~jCFTm-0G7z6jA7EJGQiIMvxZ5nJT1FZwl~QpSU7QNcS78jjKqDaKVtvCvCeOgImdgucO0QS~8Ov1M8polavWEQgMBADcKbpfGRXKWFBqIykGuG8L9PW-37XeunNP5EutZXa6VZNYgEfqtBGy3NkKNfOWEDhZvHu~ocou79QMhA__",
+      "https://s3-alpha-sig.figma.com/img/1a51/7a5d/2d74853390dc1f87ff19438a1a085b39?Expires=1714953600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=E7gsL~ooptbetarV-E2LeDD90ObA~Xbx7~qOz5L1G8O9Gu9fgLFiREnM8u0w218GWLhGotkBAXiENGRE3XWcy3oZhbAbOlCDRVH0ue~ozzisxrWdE4fXziow3ar9rTzakEoBHUS-cSc~GxRNQ9j~k9M3jc6TZD7ZUYPnmQkBDtcP2jSpfBXWgAyzo~aFHzDnKVUCJPU41p5uJJYgW7-LCjtH7YYVQDeqhPGM6~~dJZjuLUzgcNb-bJ5Ue48Ngi3K4DjUuY3Di6QqlfYnLTO0G3XjfceW8-W2C686Mxa~azTDnSnKgAb6JXtgA5TMZLI04-I3~TOlMihsdqRCmKP6xA__",
+    className: "rotate-[2.45deg]",
   },
 ];
 
 const LunchPage = () => {
-
   const t = useTranslations("Home");
   return (
     <div className="relative overflow-visible flex flex-col min-h-[calc(100vh-110px)] items-center content-container pb-24">
@@ -63,11 +63,11 @@ const LunchPage = () => {
                 </div>
                 <div className="w-full h-[120px] overflow-hidden rounded-sm">
                   <Image
-                  objectFit="cover"
-                  layout="fill"
+                    objectFit="cover"
+                    layout="fill"
                     src={v.image}
                     alt="lunch_img "
-                    className="w-full h-[140px] block object-cover"
+                    className={`w-full h-[140px] block object-cover ${v.className}`}
                   />
                 </div>
               </div>
