52dcc94638b0eec6f2768c9102efb1a627883a91
locle <locle2001@gmail.com>
2024-05-12 12:36:19 +0700

    add cf

--- src/app/[locale]/(customer)/cafe/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ComingSoon from "../../../../error/ComingSoon";
-
-export const dynamic = "force-dynamic";
-const page = () => {
-  return <ComingSoon />;
-};
-
-export default page;
