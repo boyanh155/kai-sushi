0c8a89ca371ecae57d50b8f0b324dfd3ec0dc42d
boyanh155 <locle2001@gmail.com>
2024-05-24 21:39:47 +0700

    add lunch

--- src/components/Home/HomeMain.tsx
+++ src/components/Home/HomeMain.tsx
@@ -27,7 +27,7 @@ const HomeMain = () => {
       </h1>
       {/* --------------MENU-------------- */}
       {/* Button group - MENU*/}
-      <div className="flex flex-col items-center mt-16 gap-2  ">
+      <div className="flex flex-col items-center mt-8 gap-2  ">
         {/* CAFE */}
         <div className="golden-title font-bold text-xl">
           {t("cafe_label")}&nbsp;|&nbsp;
@@ -39,8 +39,18 @@ const HomeMain = () => {
         >
           {t("beverage")}
         </div>
+        {/* LUNCH BOX */}
+        <div className="golden-title font-bold text-xl mt-6">
+          {t("lunch_label")} | <span className="uppercase">11am - 2pm</span>
+        </div>
+        <div
+          onClick={() => moveToListMenu("lunch")}
+          className="active:bg-[#8C773E99] active:border-0 font-light uppercase hover:opacity-60 transition-all cursor-pointer border border-[#B7B4B1] text-base rounded-sm p-4 w-56 text-center"
+        >
+          {t("lunch")}
+        </div>
         {/* MAIN MENU */}
-        <div className="golden-title font-bold text-xl mt-20">
+        <div className="golden-title font-bold text-xl mt-6">
           {t("dinner_label")}&nbsp;|&nbsp;
           <span className="uppercase">4pm - 10pm</span>
         </div>
@@ -62,10 +72,10 @@ const HomeMain = () => {
 
       {/* BOUNCE SCROLL */}
       <div
-        onClick={(_) => {
-          addressRef?.current?.scrollIntoView({ behavior: "smooth" });
-        }}
-        className="mt-14 p-4"
+        // onClick={(_) => {
+        //   addressRef?.current?.scrollIntoView({ behavior: "smooth" });
+        // }}
+        className="mt-6 p-4"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
